4eb5c7c2054585222a1b607a26dcc25c32074250
Whyk <lala9020@naver.com>
2018-06-20 14:39:51 +0900

    aopcal 프로젝트 사전작업

---
+++ src/main/java/kr/hs/emirim/whyk/aopcal/cal/RecCalculator.java
@@ -0,0 +1,18 @@
+package kr.hs.emirim.whyk.aopcal.cal;
+
+public class RecCalculator implements Calculator{
+	@Override
+	public long factorial(long num) {
+		long start = System.currentTimeMillis();
+		
+		try {
+			if(num == 0) 
+				return 1;
+			else 
+				return num * factorial(num-1);
+		} finally {
+			long end = System.currentTimeMillis();
+			System.out.printf("Imp factorial(%d); 실행시간 = %d", num, (end - start));
+		}
+	}
+}
