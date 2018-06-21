059ea309a37a871743cc862d50cc6af5e5250323
Whyk <lala9020@naver.com>
2018-06-21 15:11:14 +0900

    실행시간 측정하기 4 - 프록시 객체로 시간 측정

--- src/main/java/kr/hs/emirim/whyk/aopcal/main/MainCall.java
+++ src/main/java/kr/hs/emirim/whyk/aopcal/main/MainCall.java
@@ -6,20 +6,20 @@ import kr.hs.emirim.whyk.aopcal.cal.RecCalculator;
 
 public class MainCall {
 	public static void main(String[] args) {
-		long num = 10000;
+		long num = 1000;
 		
-		long start1 = System.nanoTime();
+		long start1 = System.currentTimeMillis();
 		Calculator cal1 = new ImpCalculator();
 		long f1 = cal1.factorial(num);
-		long end1 = System.nanoTime();
+		long end1 = System.currentTimeMillis();
 		System.out.printf("Imp factorial(%d); 실행시간 = %d", num, (end1 - start1));
 		
 		System.out.println();
 		
-		long start2 = System.nanoTime();
+		long start2 = System.currentTimeMillis();
 		Calculator cal2 = new RecCalculator();
 		long f2 = cal2.factorial(num);
-		long end2 = System.nanoTime();
+		long end2 = System.currentTimeMillis();
 		System.out.printf("Imp factorial(%d); 실행시간 = %d", num, (end2 - start2));
 	}
 }
